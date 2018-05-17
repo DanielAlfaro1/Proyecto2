7b9aca34950f7ab64d0a688aec3d8fe648edc87e
Bminas <Bminas@Dalfaro117>
2018-05-16 19:18:36 -0600

    Main del programa

---
+++ src/proyecto2_201700857/Proyecto2_201700857.java
@@ -0,0 +1,16 @@
+package proyecto2_201700857;
+
+
+public class Proyecto2_201700857 {
+
+    
+    public static Principal_201700857 Ventana1;
+    public static Lista_User Usuarios;
+    
+    public static void main(String[] args) {
+        Ventana1 = new Principal_201700857();
+        Usuarios = new Lista_User();
+        
+    }
+    
+}

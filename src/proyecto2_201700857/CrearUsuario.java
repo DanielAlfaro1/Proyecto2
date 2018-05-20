55257f6056d807f676b99aac4d5b61bdfea32744
Bminas <Bminas@Dalfaro117>
2018-05-19 23:11:27 -0600

    Cambios en la opción de desplazamiento o busqueda

--- src/proyecto2_201700857/CrearUsuario.java
+++ src/proyecto2_201700857/CrearUsuario.java
@@ -8,31 +8,43 @@ public class CrearUsuario {
     public JFrame Ventana;
     public JPanel P1;
     public JButton Crear, Cancelar;
-    public JLabel Titulo, usuario, contraseña, cui, nombre, correo;
+    public JLabel Titulo, usuario, contraseña, cui, nombre, correo,Fondo;
     public JTextField User, Pass, Cui, Name, Email;
     
     public void CrearPanel(){
         //Addons
         Font font = new Font("Arial Black",Font.BOLD,13);
         //Label
+        Fondo = new JLabel();
+        Fondo.setBounds(0,0,500,450);
+        ImageIcon Imagen = new ImageIcon("C:/Users/Bminas/Desktop/Imagenes/Portada.jpg");
+        Icon icono = new ImageIcon(Imagen.getImage().getScaledInstance(Fondo.getWidth(),Fondo.getHeight(),Image.SCALE_DEFAULT));
+        Fondo.setIcon(icono);        
+        
         Titulo = new JLabel("CREACIÓN DE USUARIOS");
         Titulo.setFont(font);
         Titulo.setBounds(160,20,200,25);
+        Titulo.setForeground(Color.white);
         usuario = new JLabel("USUARIO");
         usuario.setFont(font);
         usuario.setBounds(20,70,100,25);
+        usuario.setForeground(Color.white);
         contraseña = new JLabel("CONTRASEÑA");
         contraseña.setFont(font);
         contraseña.setBounds(20,120,200,25);
+        contraseña.setForeground(Color.white);
         cui = new JLabel("CUI");
         cui.setFont(font);
         cui.setBounds(20,170,100,25);
+        cui.setForeground(Color.white);
         nombre = new JLabel("NOMBRE");
         nombre.setFont(font);
         nombre.setBounds(20,220,100,25);
+        nombre.setForeground(Color.white);
         correo = new JLabel("CORREO ELECTRÓNICO");
         correo.setFont(font);
         correo.setBounds(20,270,200,25);
+        correo.setForeground(Color.white);
         //Campos de texto
         User = new JTextField();
         User.setFont(font);
@@ -61,25 +73,27 @@ public class CrearUsuario {
         //Panel y agregar
         P1 = new JPanel();
         P1.setLayout(null);
-        P1.add(Titulo);
-        P1.add(usuario);
-        P1.add(contraseña);
-        P1.add(cui);
-        P1.add(nombre);
-        P1.add(correo);
-        P1.add(User);
-        P1.add(Pass);
-        P1.add(Cui);
-        P1.add(Name);
-        P1.add(Email);
-        P1.add(Crear);
-        P1.add(Cancelar);
+        P1.add(Fondo);
+        Fondo.add(Titulo);
+        Fondo.add(usuario);
+        Fondo.add(contraseña);
+        Fondo.add(cui);
+        Fondo.add(nombre);
+        Fondo.add(correo);
+        Fondo.add(User);
+        Fondo.add(Pass);
+        Fondo.add(Cui);
+        Fondo.add(Name);
+        Fondo.add(Email);
+        Fondo.add(Crear);
+        Fondo.add(Cancelar);
         
     }
     
     public void CrearVentana(){
         Ventana = new JFrame("CREAR USUARIO");
         Ventana.setBounds(500,100,500,450);
+        Ventana.setUndecorated(true);
         Ventana.setVisible(false);
         Ventana.add(P1);
         Ventana.setDefaultCloseOperation(Ventana.HIDE_ON_CLOSE);
@@ -104,9 +118,9 @@ public class CrearUsuario {
                 }
             }
             if(e.getSource()==Cancelar){
-//                Ventana.setVisible(false);
-                    System.out.println("Nombre"+Proyecto2_201700857.Usuarios.ImprimirNombre()+" Correo"+Proyecto2_201700857.Usuarios.ImprimirEmail()+" Contraseña"+Proyecto2_201700857.Usuarios.ImprimirPass()+" cui"+Proyecto2_201700857.Usuarios.ImprimirCui()+" Usuario"+Proyecto2_201700857.Usuarios.ImprimirUsuario());
-                    Proyecto2_201700857.Usuarios.Siguiente();
+                Ventana.setVisible(false);
+//                    System.out.println("Nombre"+Proyecto2_201700857.Usuarios.ImprimirNombre()+" Correo"+Proyecto2_201700857.Usuarios.ImprimirEmail()+" Contraseña"+Proyecto2_201700857.Usuarios.ImprimirPass()+" cui"+Proyecto2_201700857.Usuarios.ImprimirCui()+" Usuario"+Proyecto2_201700857.Usuarios.ImprimirUsuario());
+                    Proyecto2_201700857.Usuarios.ActualizarB();
             }
         }
 
